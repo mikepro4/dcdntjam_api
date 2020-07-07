54613e1fa25facd1cbd92d978780978bd90f9e56
Mikhail <pmp@nopreset.ru>
2020-07-06 23:43:57 -0400

    initial api setup with CRUD and auth

---
+++ config/dev.js
@@ -0,0 +1,7 @@
+module.exports = {
+	googleClientID:
+		"915125837750-a42gg4duui3vs6530tccqd2piefg1jgr.apps.googleusercontent.com",
+	googleClientSecret: "7i7xslRn3xmUMeZCrvxZLPli",
+	mongoURI: "mongodb://dcdnt:Octatrack2$@ds157682.mlab.com:57682/dcdntjam",
+	cookieKey: "hype"
+};
